2fb35dccaa7ce96d116b2ffea4d3162b23f8fe8b
Wendy <wendy.hj.wu@gmail.com>
2018-10-09 14:39:37 +0800

    create user model using devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_07_174848) do
+ActiveRecord::Schema.define(version: 2018_10_09_063228) do
 
   create_table "posts", force: :cascade do |t|
     t.integer "user_id"
@@ -23,6 +23,22 @@ ActiveRecord::Schema.define(version: 2018_10_07_174848) do
     t.string "permission"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "name"
+    t.string "avatar"
+    t.text "introduction"
+    t.string "role"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
