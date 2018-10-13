db811b1c24e25eb827da0792feabe6fd3b97f1e2
Wendy <wendy.hj.wu@gmail.com>
2018-10-14 00:49:37 +0800

    create model category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_09_063228) do
+ActiveRecord::Schema.define(version: 2018_10_13_164819) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.integer "user_id"
