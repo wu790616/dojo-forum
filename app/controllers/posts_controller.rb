216c7d9f60c3523283bab87c5ef9908a67a36269
Wendy <wendy.hj.wu@gmail.com>
2018-10-15 23:58:19 +0800

    create edit post page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
+  before_action :set_post, only: [:show, :edit, :update]
 
   def index
     @posts = Post.all
@@ -14,15 +15,27 @@ class PostsController < ApplicationController
     @post = Post.new(post_params)
     @post.user = current_user
     @post.save
+    redirect_to post_path(@post)
   end
 
   def show
-    @post = Post.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    @post.update(post_params)
+    redirect_to post_path(@post)
   end
 
   private
 
   def post_params
     params.require(:post).permit(:title, :content, :draft, {category_ids:[]}, :image, :permission)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end  
 end
