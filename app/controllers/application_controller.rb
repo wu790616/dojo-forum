64c4ad658e01f0dea711881e8ebe23b192448bd6
Wendy <wendy.hj.wu@gmail.com>
2018-10-17 00:39:14 +0800

    add name text_field to registraion page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
 
   private
@@ -8,5 +9,9 @@ class ApplicationController < ActionController::Base
       flash[:alert] = "Not allow!"
       redirect_to root_path
     end
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 end
