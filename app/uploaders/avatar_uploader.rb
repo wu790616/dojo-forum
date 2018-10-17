6eee6da7a67be377dc209edbf023c29957d5e969
Wendy <wendy.hj.wu@gmail.com>
2018-10-17 15:06:42 +0800

    style post show page: user table

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -20,6 +20,9 @@ class AvatarUploader < CarrierWave::Uploader::Base
   #
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
+  def default_url(*args)
+    ActionController::Base.helpers.asset_path([version_name, "default.png"].compact.join('_'))
+  end
 
   # Process files as they are uploaded:
   # process scale: [200, 300]
