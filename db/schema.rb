653969d1b8747fbe343f3c461c77caaad8948670
Wendy <wendy.hj.wu@gmail.com>
2018-10-19 12:00:22 +0800

    add role default to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_17_074514) do
+ActiveRecord::Schema.define(version: 2018_10_19_034625) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -55,7 +55,7 @@ ActiveRecord::Schema.define(version: 2018_10_17_074514) do
     t.string "name", null: false
     t.string "avatar"
     t.text "introduction"
-    t.string "role"
+    t.string "role", default: "normal"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
