8a4d04c2cb2292f5f79195bf43a23195fe449e01
Wendy <wendy.hj.wu@gmail.com>
2018-10-22 15:28:30 +0800

    collect/uncollect ajax

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -53,13 +53,11 @@ class PostsController < ApplicationController
 
   def collect
     @post.collects.create!(user: current_user)
-    redirect_back(fallback_location: root_path)
   end
 
   def uncollect
     collects = Collect.where(post: @post, user: current_user)
     collects.destroy_all
-    redirect_back(fallback_location: root_path)
   end
 
   private
