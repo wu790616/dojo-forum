b77e05bb8f6c3ec85bb82afed77c18ddb28d37ac
Wendy <wendy.hj.wu@gmail.com>
2018-10-22 20:27:46 +0800

    reply author can edit and delete reply in post show page

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   end
 
   resources :posts do
-    resources :replies, only: [:create, :destroy]
+    resources :replies, only: [:create]
     
     member do
       post :collect
