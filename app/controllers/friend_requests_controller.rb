bc620145a00a823070c7b70b9d5a0413a3be1f6f
Wendy <wendy.hj.wu@gmail.com>
2018-10-23 15:31:19 +0800

    friend request create

--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -1,2 +1,17 @@
 class FriendRequestsController < ApplicationController
+  before_action :set_friend_request, only: [:update, :destroy]
+
+  def create
+    friend = User.find(params[:friend_id])
+    puts friend
+    @frend_request = current_user.friend_requests.new(friend: friend)
+    @frend_request.save
+    redirect_back(fallback_location: root_path)
+  end
+
+  private
+
+  def set_friend_request
+    @frend_request = FriendRequest.find(params[:id])
+  end
 end
