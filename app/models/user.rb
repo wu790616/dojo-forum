779508890db98e40f5ea1217b48a6a63e9b157e9
Wendy <wendy.hj.wu@gmail.com>
2018-10-23 16:25:55 +0800

    setup inverse_friend_requests in User

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,9 +17,14 @@ class User < ApplicationRecord
   has_many :collects, dependent: :destroy
   has_many :collected_posts, through: :collects, source: :post
   
-  # 一個使用者有多個朋友邀請
+  # 一個使用者可發出多個朋友邀請
   has_many :friend_requests, dependent: :destroy
   has_many :pending_friends, through: :friend_requests, source: :friend
+
+  # 一個使用者可被多個使用者邀請
+  has_many :inverse_friend_requests, class_name: "FriendRequest", foreign_key: "friend_id"
+  has_many :request_friends, through: :inverse_friend_requests, source: :user
+
 
   # 檢查是否為管理員
   def admin?
