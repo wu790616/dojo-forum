18ebd6e75aa890a32b2f1c9be527839977fd87d4
Wendy <wendy.hj.wu@gmail.com>
2018-10-24 00:31:16 +0800

    add some validate to avoid user add self as friend, hide some information in user show page

--- app/models/friend_request.rb
+++ app/models/friend_request.rb
@@ -1,9 +1,19 @@
 class FriendRequest < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: "User"
+  validates :user, presence: true
+  validates :friend, presence: true, uniqueness: { scope: :user }
+  validate :disallow_self
 
   def accept
     user.friends << friend
     destroy
+  end
+
+  # 自訂驗證
+  def disallow_self
+    if user_id == friend_id
+      errors[:friend_id] << 'cannot add self as friend'
+    end
   end
 end
