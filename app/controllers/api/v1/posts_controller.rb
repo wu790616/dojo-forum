a88f89db1ec4d8991e1f4e4b0ad93b56feb64748
Wendy <wendy.hj.wu@gmail.com>
2018-10-25 14:51:16 +0800

    add api::v1::post show

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,6 +1,29 @@
 class Api::V1::PostsController < ApiController
+  before_action :set_post, only: [:show]
+
   def index
     @posts = Post.open
-    render json: @posts
+    render json: {
+      data: @posts
+    }
+  end
+
+  def show
+    if !@post
+      render json: {
+        message: "Can't find this post.",
+        status: 404
+      }
+    else
+      render json: {
+        data: @post
+      }
+    end
+  end
+
+  private
+
+  def set_post
+    @post = Post.find_by(id: params[:id])
   end
 end
